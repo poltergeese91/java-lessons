9c276e37ea796cbbb2b5e9093e99882fe300021e
Hunter Keeth <hjkeeth@gmail.com>
2026-01-14 16:58:23 +0000

    commit 1/14

---
+++ OracleArrayPractice.java
@@ -0,0 +1,41 @@
+public class OracleArrayPractice {
+    //7654
+    //2312 \n 2102 \n 3104
+    // 2
+    //   0  3
+    //   1  4 
+    //   2  5
+    //  7
+    //  14
+    //  11
+    //11 
+    //5
+    public static void main(String[] args)
+    {
+        double[] winners = new double[18];
+
+        int[] first_array = new int[]{2, 3, 5, 7, 11, 13, 17, 19};
+
+        int[] doubles = new int[20];
+        
+        for (int i = 0; i < doubles.length; i++)
+        {
+            doubles[i] = i * 2;
+            System.out.println(doubles[i]);
+        }
+        int[][] maze = new int[30][40];
+
+        int[][] value = new int[16][32]
+
+        for (int h = 0; h < value.length; h++)
+        {
+            for (int g = 0; g < value[h].length; g++)
+                System.out.println()
+
+
+        }
+
+
+    }
+
+}   
---
+++ TwoDTester.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TwoDTester {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        int[][] nums = new int[3][2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.println("Enter a value for row: " + i + ", and columnn: " + j);
+                nums[i][j] = input.nextInt();
+            }
+        }
+        input.close();
+      
+
+        for (int j = 0; j < nums.length; j++) {
+            for (int i = 0; i < nums[i].length; i++) {
+                System.out.print(nums[j][i] + " ");
+            } 
+            System.out.println();
+        }
+        input.close();
+    }
+}
